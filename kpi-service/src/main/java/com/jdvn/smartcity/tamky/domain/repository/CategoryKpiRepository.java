19152f05a2d5bcfd3bd3f620996a99ced509066d
Hoang Vinh Thai <thaihv@gmail.com>
2021-07-28 18:04:02 +0700

    Refine models

--- kpi-service/src/main/java/com/jdvn/smartcity/tamky/domain/repository/CategoryKpiRepository.java
+++ kpi-service/src/main/java/com/jdvn/smartcity/tamky/domain/repository/CategoryKpiRepository.java
@@ -3,8 +3,8 @@ package com.jdvn.smartcity.tamky.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.jdvn.smartcity.tamky.domain.model.ClassType;
+import com.jdvn.smartcity.tamky.domain.model.CategoryKpi;
 
 @RepositoryRestResource
-public interface SubCategoryRepository extends JpaRepository<ClassType, Long> {
+public interface CategoryKpiRepository extends JpaRepository<CategoryKpi, Long> {
 }
