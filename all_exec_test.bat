c055d441abca264f2ef4b54fe7da3bd3f28fd1b7
thaihv <thaihv@gmail.com>
2021-09-28 13:24:12 +0700

    ultilities bat for dev easily

---
+++ all_exec_test.bat
@@ -0,0 +1,4 @@
+start /d ".\discovery-service" mvnw spring-boot:run
+start /d ".\api-gateway" mvnw spring-boot:run
+start /d ".\realtime-service" mvnw spring-boot:run
+start /d ".\ui-community-service" mvnw spring-boot:run
