41e8655e643a0f7d07e5c6d921482d7808f7fe12
thaihv <thaihv@gmail.com>
2021-11-03 15:33:20 +0700

    Add REST API to geo-service using flask python

--- geo-service/analysisApp/forms.py
+++ geo-service/analysisApp/forms.py
@@ -1,5 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField
+from wtforms import SelectField, TextField, FloatField
 class ArenaForm(FlaskForm):
     description = "Use the dropdown to select an arena."
     selections = SelectField('Select an Arena',choices=[])
+
+class AddForm(FlaskForm):
+	name = TextField('Arena Name')
+	longitude = FloatField('Longitude')
+	latitude = FloatField('Latitude')
