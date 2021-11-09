6b3c72c8dff8aa24604cfb2ef083b634462cb463
thaihv <thaihv@gmail.com>
2021-11-09 16:55:06 +0700

    Update wsgi for geo service

--- geo-service/application/__init__.py
+++ geo-service/application/__init__.py
@@ -8,7 +8,7 @@ import py_eureka_client.eureka_client as eureka_client
 
 app = flask.Flask(__name__)
 
-conn_string = 'postgresql://postgres:postgres@localhost:5432/geospatial'
+conn_string = 'postgresql://postgres:postgres@www.tamky.xyz:25432/geospatial'
 # app.config['SQLALCHEMY_DATABASE_URI'] = conn_string
 # app.config['SECRET_KEY'] = "SECRET_KEY"
 # app.config['DEBUG'] = True
